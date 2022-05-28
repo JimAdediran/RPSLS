d3052c900d73817980dc5bb87d9dece2a061f994
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-28 14:40:47 -0400

    player.py class created and used as parent class inheritance for human and ai classes, game_initiate funciton updateed in game.py

---
+++ human.py
@@ -0,0 +1,3 @@
+from player import Player
+import random
+from time import sleep
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, identity):
+        self.score = 0
+        self.identity = identity 
+        
