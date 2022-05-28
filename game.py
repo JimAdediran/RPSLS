d0719012cc38973214e82ddf2d1131166ccba1af
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-28 13:59:51 -0400

    functions game_initiate and number_of_players added to game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+def game_initiate():
+    print("yes")
+    
+    
+print("Welcome to Rock Paper Scissors Lizard Spock \n \neach match will be best of three games \
+\nUse the number keys to enter your selection \n \n \nScissors cuts Paper \nPaper covers Rock \nRock crushes Lizard \
+\nLizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock \
+\nSpock vaporizes Rock \nRock crushes Scissors")
+   
