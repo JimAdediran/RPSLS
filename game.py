0aa66ee35ee9e4b5b541dcf4c90898663d69646b
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-28 18:28:06 -0400

    logic for starting game in main.py and number_of_players function in game.py updated to include input

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
+from time import sleep
+
 def game_initiate():
+    sleep(2)
     print("Welcome to Rock Paper Scissors Lizard Spock \n \neach match will be best of three games \
 \nUse the number keys to enter your selection \n \n \nScissors cuts Paper \nPaper covers Rock \nRock crushes Lizard \
 \nLizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock \
-\nSpock vaporizes Rock \nRock crushes Scissors")
+\nSpock vaporizes Rock \nRock crushes Scissors \n")
 
 def number_of_players():
+    sleep(2)
     print("How many players? 1, 2 or 3 for a surprise")
+    player_number = input()
 
+    
 
-   
---
+++ main.py
@@ -0,0 +1,5 @@
+import game
+
+game_one = game.game_initiate()
+
+game.number_of_players()
